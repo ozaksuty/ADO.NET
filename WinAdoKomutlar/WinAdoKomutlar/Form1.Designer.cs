ac85cb5b915f069549fda462c8c96f95c9e42f41
Yiğit <ozaksuty@gmail.com>
2016-04-14 23:27:49 +0300

    ADO.NET Komutlarına Giriş
    ADO.NET Komutlarına Giriş

---
+++ WinAdoKomutlar/WinAdoKomutlar/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WinAdoKomutlar
+{
+    partial class frmADONET
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetCategories = new System.Windows.Forms.Button();
+            this.cmbCategories = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblProductCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGetCategories
+            // 
+            this.btnGetCategories.Location = new System.Drawing.Point(12, 12);
+            this.btnGetCategories.Name = "btnGetCategories";
+            this.btnGetCategories.Size = new System.Drawing.Size(133, 23);
+            this.btnGetCategories.TabIndex = 0;
+            this.btnGetCategories.Text = "Get Categories";
+            this.btnGetCategories.UseVisualStyleBackColor = true;
+            this.btnGetCategories.Click += new System.EventHandler(this.btnGetCategories_Click);
+            // 
+            // cmbCategories
+            // 
+            this.cmbCategories.FormattingEnabled = true;
+            this.cmbCategories.Location = new System.Drawing.Point(152, 13);
+            this.cmbCategories.Name = "cmbCategories";
+            this.cmbCategories.Size = new System.Drawing.Size(121, 21);
+            this.cmbCategories.TabIndex = 1;
+            this.cmbCategories.SelectedIndexChanged += new System.EventHandler(this.cmbCategories_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Product Count : ";
+            // 
+            // lblProductCount
+            // 
+            this.lblProductCount.AutoSize = true;
+            this.lblProductCount.Location = new System.Drawing.Point(149, 61);
+            this.lblProductCount.Name = "lblProductCount";
+            this.lblProductCount.Size = new System.Drawing.Size(13, 13);
+            this.lblProductCount.TabIndex = 3;
+            this.lblProductCount.Text = "0";
+            // 
+            // frmADONET
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 169);
+            this.Controls.Add(this.lblProductCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbCategories);
+            this.Controls.Add(this.btnGetCategories);
+            this.Name = "frmADONET";
+            this.Text = "ADO.NET";
+            this.Load += new System.EventHandler(this.frmADONET_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetCategories;
+        private System.Windows.Forms.ComboBox cmbCategories;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblProductCount;
+    }
+}
+
---
+++ WinAdoKomutlar/WinAdoKomutlar/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAdoKomutlar
+{
+    public partial class frmADONET : Form
+    {
+        public frmADONET()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cnn = new SqlConnection("Server=.;Database=NORTHWND;Trusted_Connection=True;");
+        private void frmADONET_Load(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM Products", cnn);
+            cnn.Open();
+            int productCount = (int)cmd.ExecuteScalar();
+            lblProductCount.Text = productCount.ToString();
+            cnn.Close();
+        }
+
+        private void btnGetCategories_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Categories", cnn);
+            cnn.Open();
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            List<Categories> categoryList = new List<Categories>();
+            
+            while (dr.Read())
+            {
+                categoryList.Add(new Categories() { CategoryID = (int)dr["CategoryID"],
+                    CategoryName = dr["CategoryName"].ToString() });
+            }
+
+            cnn.Close();
+
+            cmbCategories.DataSource = categoryList;
+            cmbCategories.DisplayMember = "CategoryName";
+            cmbCategories.ValueMember = "CategoryID";
+        }
+
+        class Categories
+        {
+            public int CategoryID { get; set; }
+            public string CategoryName { get; set; }
+        }
+
+        private void cmbCategories_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox combo = (ComboBox)sender;
+            Categories selectedCategory = (Categories)combo.SelectedItem;
+
+            SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM Products WHERE CategoryID=" + 
+                selectedCategory.CategoryID, cnn);
+            cnn.Open();
+            int productCount = (int)cmd.ExecuteScalar();
+            lblProductCount.Text = productCount.ToString();
+            cnn.Close();
+        }
+    }
+}
